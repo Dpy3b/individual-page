4093dd0e3d5ae8b91007840555e28ffc79458608
Dpy3b <57526052+Dpy3b@users.noreply.github.com>
2021-11-05 11:25:00 +0300

    v.1.0.7

--- src/js/main.js
+++ src/js/main.js
@@ -5,6 +5,30 @@
   * @returns {number}
   */
 
+
+
+
+// Найти все ссылки начинающиеся на #
+const anchors = document.querySelectorAll('a[href^="#"]')
+
+// Цикл по всем ссылкам
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault() // Предотвратить стандартное поведение ссылок
+    // Атрибут href у ссылки, если его нет то перейти к body (наверх не плавно)
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    // Плавная прокрутка до элемента с id = href у ссылки
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+  });
+};
+
+
+
+ ////////
+
  jQuery(($) => {
 	$('.select').on('click', '.select__head', function () {
 		if ($(this).hasClass('open')) {
@@ -235,3 +259,7 @@ $('.exsamples-of-civil').mouseleave(function() {
     if (bg) $(this).parents('.examples-foundation').css('background', '');
 });
  */
+/////
+
+
+
